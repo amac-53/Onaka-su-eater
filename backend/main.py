f424c2b42d729b3fe2d589d785306933ba0fb6f8
amane <amachonchon@gmail.com>
2022-11-16 19:43:55 +0900

    fixed some bugs and changed how to receive json data

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,8 @@
 from fastapi import FastAPI, Response
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.encoders import jsonable_encoder
 from fastapi.responses import JSONResponse
 import requests
 import json
-import xmltodict
 import config
 
 api_key = config.HOTPEPPER_API_KEY
@@ -14,7 +12,6 @@ app = FastAPI()
 
 origins = [
     "*"
-    # "http://localhost",
 ]
 
 app.add_middleware(
@@ -29,10 +26,15 @@ app.add_middleware(
 async def root():
     return {"message": "Hello World"}
 
-# return json
-# http://127.0.0.1:8000/items/?latitude=34.910047&longitude=135.7805467&range=300
+
 @app.get("/items/")
 async def get_items(latitude: float = 0, longitude: float = 0, range: int = 1, count: int = 100):
+    """
+    現在地付近の店の情報を全て返す
+    range: 現在地からの距離
+    count: 店の数の上限
+    """
+
     range_list =  {300: 1, 500: 2, 1000: 3, 2000: 4, 3000: 5}
     base_url = 'http://webservice.recruit.co.jp/hotpepper/gourmet/v1/'
 
@@ -41,26 +43,27 @@ async def get_items(latitude: float = 0, longitude: float = 0, range: int = 1, c
     'lat': str(latitude),
     'lng': str(longitude),
     'range': str(range_list[range]),
-    'count': str(count)
+    'count': str(count),
+    'format': 'json'
     }
 
     res = requests.get(base_url, query)
-    dict_data = xmltodict.parse(res.text)  # XML to dict
-    # return Response(content=res.text, media_type="application/xml")
-    return dict_data
+    return res.json()
 
-# http://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=296aec41c1f8b322&lat=34.67&lng=135.52&range=5&order=4
 
-# 各店の詳細情報を取得
 @app.get("/items/detail/")
 async def get_items(id: str):
+    """
+    特定の店の情報を返す
+    id: 店を一意に識別するid
+    """
+
     base_url = 'http://webservice.recruit.co.jp/hotpepper/gourmet/v1/'
     query = {
     'key': api_key,
     'id': id,
+    'format': 'json'
     }
 
     res = requests.get(base_url, query)
-    dict_data = xmltodict.parse(res.text)  # XML to dict
-    # return Response(content=res.text, media_type="application/xml")
-    return dict_data
+    return res.json()
