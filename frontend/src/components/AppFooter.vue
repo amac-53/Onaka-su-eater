301c6cc1f2210b8f143d69403c1f01163e32a445
amane <amachonchon@gmail.com>
2022-11-24 15:38:12 +0900

    added some changes about terms of service

--- frontend/src/components/AppFooter.vue
+++ frontend/src/components/AppFooter.vue
@@ -1,5 +1,8 @@
 <template>
     <footer class="sticky-top" style="background: linear-gradient(to top, rgb(247 147 6), rgb(250 208 104));">
-       <small class="text-white">Copyright(c) Amane Yamamoto All Rights Reserved.</small>
+        <div class="d-flex justify-content-between">
+            <small class="text-white">Copyright(c) Amane Yamamoto All Rights Reserved.</small>
+            <small class="text-white">株式会社リクルートより情報提供</small>
+        </div>
     </footer>
 </template>
